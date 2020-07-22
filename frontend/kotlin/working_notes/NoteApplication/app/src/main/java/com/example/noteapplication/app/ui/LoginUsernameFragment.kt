57f5ce4bb487e9dea1ff9237d2f08c7d8d23d3da
Tim Smith <tijak@protonmail.ch>
2020-07-22 12:33:49 +0100

    add navcomponent

--- frontend/kotlin/working_notes/NoteApplication/app/src/main/java/com/example/noteapplication/app/ui/LoginUsernameFragment.kt
+++ frontend/kotlin/working_notes/NoteApplication/app/src/main/java/com/example/noteapplication/app/ui/LoginUsernameFragment.kt
@@ -1,8 +1,16 @@
 package com.example.noteapplication.app.ui
 
 import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import com.example.noteapplication.R
 import com.example.noteapplication.app.viewmodel.NoteViewModel
+import com.example.noteapplication.databinding.LoginUsernameFragmentBinding
 import javax.inject.Inject
 
 class LoginUsernameFragment: Fragment() {
@@ -10,6 +18,8 @@ class LoginUsernameFragment: Fragment() {
     // Fields that need to be injected by the login graph
     @Inject
     lateinit var loginViewModel: NoteViewModel
+
+    lateinit var binding: LoginUsernameFragmentBinding
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -20,5 +30,19 @@ class LoginUsernameFragment: Fragment() {
 
         // Now you can access loginViewModel here and onCreateView too
         // (shared instance with the Activity and the other Fragment)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.login_username_fragment, container, false )
+        binding.navigateToOther.setOnClickListener {
+           this.view?.let {
+               Navigation.findNavController(it).navigate(R.id.loginPasswordFragment)
+           }
+        }
+        return binding.root
     }
 }
