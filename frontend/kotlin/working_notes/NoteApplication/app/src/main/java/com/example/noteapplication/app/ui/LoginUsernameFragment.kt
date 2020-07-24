0562770103b005e4237aed521de81b1ecbac689b
Tim Smith <tijak@protonmail.ch>
2020-07-24 16:57:12 +0100

    update to use viewmodel

--- frontend/kotlin/working_notes/NoteApplication/app/src/main/java/com/example/noteapplication/app/ui/LoginUsernameFragment.kt
+++ frontend/kotlin/working_notes/NoteApplication/app/src/main/java/com/example/noteapplication/app/ui/LoginUsernameFragment.kt
@@ -2,22 +2,16 @@ package com.example.noteapplication.app.ui
 
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.noteapplication.R
 import com.example.noteapplication.app.viewmodel.NoteViewModel
 import com.example.noteapplication.databinding.LoginUsernameFragmentBinding
-import com.example.noteapplication.domain.models.NoteModel
-import io.reactivex.Observer
-import io.reactivex.observers.DisposableSingleObserver
-import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 class LoginUsernameFragment: Fragment() {
@@ -27,9 +21,6 @@ class LoginUsernameFragment: Fragment() {
     lateinit var noteViewModel: NoteViewModel
 
     lateinit var binding: LoginUsernameFragmentBinding
-    private lateinit var viewAdapter: RecyclerView.Adapter<*>
-    private lateinit var viewManager: RecyclerView.LayoutManager
-
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -38,26 +29,10 @@ class LoginUsernameFragment: Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        noteViewModel.notes
-            .subscribeOn(Schedulers.io())
-            .subscribeWith(object: DisposableSingleObserver<List<NoteModel>>(),
-                Observer<NoteModel> {
-                override fun onSuccess(value: List<NoteModel>?) {
-                    value?.let { notes ->
-                        activity?.runOnUiThread { binding.recyclerView.adapter = NoteAdapter(notes) }
-                    }
-                    Log.d("Observer result:", "onSuccess: ${value?.size}")
-                }
+        noteViewModel.getNotes().observe(this, androidx.lifecycle.Observer { notes ->
+            binding.recyclerView.adapter = NoteAdapter(notes)
+        })
 
-                override fun onError(e: Throwable?) {}
-                override fun onComplete() {}
-                override fun onNext(value: NoteModel?) {}
-            })
-
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
     }
 
     override fun onCreateView(
--- frontend/kotlin/working_notes/NoteApplication/app/src/main/java/com/example/noteapplication/app/viewmodel/NoteViewModel.kt
+++ frontend/kotlin/working_notes/NoteApplication/app/src/main/java/com/example/noteapplication/app/viewmodel/NoteViewModel.kt
@@ -1,12 +1,47 @@
 package com.example.noteapplication.app.viewmodel
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import com.example.noteapplication.app.di.scopes.ActivityScope
+import com.example.noteapplication.app.ui.NoteAdapter
+import com.example.noteapplication.domain.models.NoteModel
 import com.example.noteapplication.domain.usecases.GetNotesUseCase
+import io.reactivex.Observer
+import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 @ActivityScope
 class NoteViewModel @Inject constructor(
-    notesUseCase: GetNotesUseCase
-) {
-    val notes = notesUseCase.execute()
+    val notesUseCase: GetNotesUseCase
+): ViewModel() {
+    private val notes: MutableLiveData<List<NoteModel>> by lazy {
+        MutableLiveData<List<NoteModel>>().also {
+            loadNotes()
+        }
+    }
+
+    fun getNotes(): LiveData<List<NoteModel>> {
+        return notes
+    }
+
+    private fun loadNotes() {
+        notesUseCase.execute()
+            .subscribeOn(Schedulers.io())
+            .subscribeWith(object: DisposableSingleObserver<List<NoteModel>>(),
+                Observer<NoteModel> {
+                override fun onSuccess(obtainedNotes: List<NoteModel>?) {
+                    notes.postValue(obtainedNotes)
+                }
+
+                override fun onError(e: Throwable?) {
+                    Log.d("onerror", e.toString())
+                }
+                override fun onComplete() {}
+                override fun onNext(value: NoteModel?) {}
+            })
+    }
+
 }
