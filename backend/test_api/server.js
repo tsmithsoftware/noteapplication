44b9e42189a7b6999241d1df8f8d743e87f8348e
Tim Smith <tijak@protonmail.ch>
2020-08-03 18:18:35 +0100

    update server

--- backend/test_api/server.js
+++ backend/test_api/server.js
@@ -23,6 +23,23 @@ const HOST = '0.0.0.0';
 const selectQuery = "SELECT * FROM public.notes"
 const deleteQuery = "DELETE FROM public.notes WHERE noteid="
 const insertQuery = "INSERT INTO public.notes (notetitle, notedetails) VALUES ("
+const updateQuery = "UPDATE public.notes SET notetitle = "
+
+function buildSelectQuery(noteId){
+	return selectQuery + " WHERE noteid=" + noteId + ";"
+}
+
+function buildDeleteQuery(noteId) {
+	return deleteQuery + noteId + ";"
+}
+
+function buildInsertQuery(noteTitle, noteDetails) {
+	return insertQuery + "'" + noteTitle + "','" + noteDetails + "');"
+}
+
+function buildUpdateQuery(noteId, newTitle, newDetails) {
+	return updateQuery + newTitle + ", notedetails = " + newDetails + " WHERE noteid = " + noteId + ";" 
+}
 
 // App
 const app = express();
@@ -44,7 +61,7 @@ app.get('/notes', (req, res) => {
 );
 
 app.get('/notes/:noteId', (req, res) => {
-	var fullQuery = selectQuery + " WHERE noteid=" + req.params["noteId"] + ";"
+	var fullQuery = buildSelectQuery(req.params["noteId"])
 	client.query(fullQuery,
 		function (err, result) {
 			if (err) {
@@ -60,7 +77,7 @@ app.get('/notes/:noteId', (req, res) => {
 );
 
 app.delete('/notes/:noteId', (req, res) => {
-	var fullQuery = deleteQuery + req.params["noteId"]
+	var fullQuery = buildDeleteQuery(req.params["noteId"])
 	console.log(fullQuery)
 	client.query(fullQuery,
 		function (err, result) {
@@ -77,10 +94,27 @@ app.delete('/notes/:noteId', (req, res) => {
 
 app.post('/notes', (req,res) => {
 	var noteObject = req.body
-	var fullQuery = insertQuery + "'" + noteObject.noteTitle + "','" + noteObject.noteDetails + "');"
+	var fullQuery = buildInsertQuery(noteObject.noteTitle, noteObject.noteDetails)
 	console.log(fullQuery)
 	
 	client.query(fullQuery,
+		function (err, result) {
+			if (err) {
+				console.log (err);
+				res.status(400).send(err);
+				} else {
+					res.status(200).send();
+				}
+		} 
+	)
+});
+
+app.put('/notes', (req,res) => {
+	var noteObject = req.body
+	var fullQuery = buildUpdateQuery(noteObject.noteId, noteObject.noteTitle, noteObject.noteDetails)
+	console.log(fullQuery)
+	
+	client.query(fullQuery, 
 		function (err, result) {
 			if (err) {
 				console.log (err);
