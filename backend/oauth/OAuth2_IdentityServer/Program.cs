60d532b6fce98ab50af0ad946be353597234c002
Tim Smith <tijak@protonmail.ch>
2020-08-04 22:38:54 +0100

    oauth server set up

---
+++ backend/oauth/OAuth2_IdentityServer/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Owin.Hosting;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OAuth2_IdentityServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // logging
+            Log.Logger = new LoggerConfiguration()
+                .CreateLogger();
+
+            using (WebApp.Start<Startup>("http://localhost:12345"))
+            {
+                Console.WriteLine("server running...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ backend/oauth/OAuth2_IdentityServer/Startup.cs
@@ -0,0 +1,30 @@
+﻿using IdentityServer3.Core.Configuration;
+using IdentityServer3.Core.Services.InMemory;
+using OAuth2_IdentityServer.Core.Models;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OAuth2_IdentityServer
+{
+    class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var options = new IdentityServerOptions
+            {
+                Factory = new IdentityServerServiceFactory()
+                            .UseInMemoryClients(Clients.Get())
+                            .UseInMemoryScopes(Scopes.Get())
+                            .UseInMemoryUsers(new List<InMemoryUser>()),
+
+                RequireSsl = false
+            };
+
+            app.UseIdentityServer(options);
+        }
+    }
+}
